0b6d103ed736281bc27b7c433fb0fd0bbe08b7da
MonsterEnergy5 <jonas.grieshammer@gmail.com>
2025-10-04 17:13:10 +0200

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <main style={{ padding: 24, fontFamily: "system-ui, sans-serif" }}>
+      <h1>BankBitz 🏦</h1>
+      <p>My first React change!</p>
+      <button onClick={() => alert("Hello from BankBitz!")}>Click me</button>
+    </main>
+  );
+}
